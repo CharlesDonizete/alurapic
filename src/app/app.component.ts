57c3a89004a8cb60c9ab2f1d45d6aa7fdf06c51f
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-02-27 22:39:50 -0300

    01. Bem começado, metade feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "alurapic";
+  description = "Leão";
+  url =
+    "https://as2.ftcdn.net/v2/jpg/05/52/16/77/1000_F_552167711_fdlTXkzMDzC39dr3dNiovQEwTNhBqKzz.jpg";
 }
