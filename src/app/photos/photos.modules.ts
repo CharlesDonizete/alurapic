532dc1a95e6abe27b8d3a1073c9468be713206b1
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-03-01 18:53:37 -0300

    03 - Integração com Web API's

--- src/app/photos/photos.modules.ts
+++ src/app/photos/photos.modules.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
 @NgModule({
   declarations: [PhotoComponent],
+  imports: [HttpClientModule],
   exports: [PhotoComponent],
 })
 export class PhotosModule {}
