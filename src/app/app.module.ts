2fc91abf4bfb5390de5a16f29213105dc2bef3a7
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-03-02 18:48:00 -0300

    04. Single Page Applications e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { PhotosModule } from "./photos/photos.modules";
+import { ErrorsModule } from "./errors/errors.module";
+import { AppRoutingModule } from "./app.routing.module";
+import { PhotosModule } from "./photos/photos.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule],
+  imports: [BrowserModule, PhotosModule, AppRoutingModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
