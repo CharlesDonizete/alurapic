56947bf588e323174e60177fa7af318fc0218406
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-04-24 21:30:24 -0300

    04. Menu da aplicação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { MenuModule } from "./../shared/components/menu/menu.module";
 import { FooterComponent } from "./footer/footer.component";
 import { RequestInterceptor } from "./auth/request.interceptor";
 import { CommonModule } from "@angular/common";
@@ -7,11 +8,19 @@ import { RouterModule } from "@angular/router";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AlertModule } from "../shared/components/alert/alert.module";
 import { LoadingModule } from "../shared/components/loading/loading.module";
+import { ShowIfLoggedModule } from "../shared/directives/show-if-logged/show-if-logged.module";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, AlertModule, LoadingModule, RouterModule],
+  imports: [
+    CommonModule,
+    AlertModule,
+    LoadingModule,
+    RouterModule,
+    MenuModule,
+    ShowIfLoggedModule,
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
